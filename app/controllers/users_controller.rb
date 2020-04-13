4b8591568e4f24c7b7df343502dccc800f762549
kaidornel <kai_dornel@hotmail.com>
2020-04-12 21:27:19 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,21 +18,3 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
-
-# class UsersController < ApplicationController
-#     def new
-#     end
-  
-#     def create
-#       @user = User.create(user_params)
-#       return redirect_to controller: 'users', action: 'new' unless @user.save
-#       session[:user_id] = @user.id
-#       redirect_to controller: 'welcome', action: 'home'
-#     end
-  
-#     private
-  
-#     def user_params
-#       params.require(:user).permit(:name, :password, :password_confirmation)
-#     end
-# end
